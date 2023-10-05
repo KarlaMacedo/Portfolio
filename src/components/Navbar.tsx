d66416ecff18847928dc799254536a9c2db7d2b8
KarlaMacedo <karlapaolamacedo@gmail.com>
2023-10-04 19:50:01 -0600

    fix collapse navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,10 +4,7 @@ const Navbar = () => {
   return (
     <nav className="navbar sticky-top navbar-expand-lg navbar-light bg-white" data-spy="affix" data-offset-top="510">
       <div className="container">
-        <button className="navbar-toggler ml-auto" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse mt-sm-20 navbar-collapse" id="navbarSupportedContent">
+        <div className="mt-sm-20 navbar-collapse" id="navbarSupportedContent">
           <ul className="navbar-nav mr-auto">
             <li className="nav-item">
               <a href="#home" className="nav-link">Inicio</a>
